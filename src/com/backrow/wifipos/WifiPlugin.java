51503ee0ba7d4038b047477659d45db77311360e
Patrick Chilton <chpatrick@gmail.com>
2012-12-29 02:58:01 +0000

    Initial commit

---
+++ src/com/backrow/wifipos/WifiPlugin.java
@@ -0,0 +1,15 @@
+package com.backrow.wifipos;
+
+import org.apache.cordova.api.CordovaPlugin;
+import org.apache.cordova.api.PluginResult;
+import org.json.JSONArray;
+
+public class WifiPlugin extends CordovaPlugin {
+
+  @Override
+  public PluginResult execute(String command, JSONArray args, String callbackId) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
